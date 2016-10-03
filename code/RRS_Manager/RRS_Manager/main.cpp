c0f02676be068f1de8e3f973fd1f6328da761eb6
Joe Cloud <im@joe.cloud>
2016-10-02 22:38:53 -0500

    deleted the cout

--- code/RRS_Manager/RRS_Manager/main.cpp
+++ code/RRS_Manager/RRS_Manager/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	cout << "testing making some changes" << endl;
+
 /* wouldn't merge, so i reset my changes and am now manually adding them with your updated commit*/
 }
 
