1f3f09db0529d6a821338c21f82994b4fb1c370d
Joe Cloud <im@joe.cloud>
2016-10-05 01:09:21 -0500

    added unknown.cpp: contains menu prints

---
+++ code/RRS_Manager/RRS_Manager/unknown.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include <iostream>
+
+/* know something like this will be needed */ 
+
+using namespace std;
+
+void welcome()
+{
+	cout 
+	<<
+	"--------------------------------------------------\n"
+	<<
+	"|        welcome to robbie robot shop 1.0        |\n"
+	<<
+	"--------------------------------------------------\n"
+	<<
+	"|     created by: brandon chase // joe cloud     |\n"
+	<<
+	"--------------------------------------------------\n";
+
+
+}
+
+void mainf()
+{
+
+}
+
+void create()
+{
+
+}
+
+void report()
+{
+
+}
+
+void save()
+{
+
+}
+
+int main()
+{
+/* function used for testing print format */
+	welcome();
+	mainf();
+	create();
+	report();
+	save();
+
+	return 0;
+}
