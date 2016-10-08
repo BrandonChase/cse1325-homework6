f38b4f24daf9b32324b1161f673917544b4fa6e2
Brandon Chase <brandonc2127@gmail.com>
2016-10-08 17:01:20 -0500

    Added some empty files and updated class diagram

--- code/RRS_Manager/RRS_Manager/robot_parts.h
+++ code/RRS_Manager/RRS_Manager/robot_parts.h
@@ -12,14 +12,14 @@ public:
 				std::string p_name,
 				int p_part_number,
 				double p_weight,
-				double p_cost,
+				double p_price,
 				PartType p_type,
 				std::string p_description) :
 
 				name(p_name),
 				part_number(p_part_number),
 				weight(p_weight),
-				cost(p_cost),
+				price(p_price),
 				type(p_type),
 				description(p_description) {}
 				
@@ -33,7 +33,7 @@ protected:
 	std::string name;
 	int part_number;
 	double weight;
-	double cost;
+	double price;
 	PartType type;
 	std::string description;
 };
@@ -45,11 +45,11 @@ public:
 			std::string p_name,
 			int p_part_number,
 			double p_weight,
-			double p_cost,
+			double p_price,
 			PartType p_type,
 			std::string p_description) :
 
-			RobotPart(p_name, p_part_number, p_weight, p_cost, p_type, p_description) {}
+			RobotPart(p_name, p_part_number, p_weight, p_price, p_type, p_description) {}
 
 private:
 };
@@ -61,14 +61,14 @@ public:
 				std::string p_name,
 				int p_part_number,
 				double p_weight,
-				double p_cost,
+				double p_price,
 				PartType p_type,
 				std::string p_description,
 				double p_max_speed,
 				double p_power_consumed) :
 
 
-				RobotPart(p_name, p_part_number, p_weight, p_cost, p_type, p_description),
+				RobotPart(p_name, p_part_number, p_weight, p_price, p_type, p_description),
 				max_speed(p_max_speed),
 				power_consumed(p_power_consumed) {}
 private:
@@ -83,12 +83,12 @@ public:
 			std::string p_name, 
 			int p_part_number, 
 			double p_weight, 
-			double p_cost, 
+			double p_price, 
 			PartType p_type, 
 			std::string p_description,
 			int p_battery_compartments) :
 		
-			RobotPart(p_name, p_part_number, p_weight, p_cost, p_type, p_description), 
+			RobotPart(p_name, p_part_number, p_weight, p_price, p_type, p_description), 
 			battery_compartments(p_battery_compartments) {}
 
 private:
@@ -102,12 +102,12 @@ public:
 				std::string p_name,
 				int p_part_number,
 				double p_weight,
-				double p_cost,
+				double p_price,
 				PartType p_type,
 				std::string p_description,
 				int p_contained_energy) :
 
-				RobotPart(p_name, p_part_number, p_weight, p_cost, p_type, p_description),
+				RobotPart(p_name, p_part_number, p_weight, p_price, p_type, p_description),
 				contained_energy(p_contained_energy) {}
 
 private:
@@ -121,12 +121,12 @@ public:
 		std::string p_name,
 		int p_part_number,
 		double p_weight,
-		double p_cost,
+		double p_price,
 		PartType p_type,
 		std::string p_description,
 		int p_power_consumed) :
 
-		RobotPart(p_name, p_part_number, p_weight, p_cost, p_type, p_description),
+		RobotPart(p_name, p_part_number, p_weight, p_price, p_type, p_description),
 		power_consumed(p_power_consumed) {}
 	
 private:
