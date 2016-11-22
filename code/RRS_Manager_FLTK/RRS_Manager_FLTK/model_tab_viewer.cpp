c222f77353404239a4fe2aa6e0ae0e5dabe95d89
Joe Cloud <im@joe.cloud>
2016-11-21 19:40:03 -0600

    ya... my fault

---
+++ code/RRS_Manager_FLTK/RRS_Manager_FLTK/model_tab_viewer.cpp
@@ -0,0 +1,75 @@
+#include "model_tab_viewer.h"
+
+#include <FL/Fl_File_Chooser.H>
+#include <stdexcept>
+#include <exception>
+#include <string>
+
+using namespace std;
+
+void ModelTabViewer::display_tabs()
+{
+	tabs = new Fl_Tabs(xoffset,yoffset,xsize+50,ysize+150);
+	{
+		Fl_Group *head_g = new Fl_Group(xoffset,yoffset,xsize,ysize+50,"Head");
+		{
+			head_picture_box = new Fl_Box(75+xoffset, 50+yoffset, 100, 100);
+		}
+		head_g->end();
+
+		Fl_Group *torso_g = new Fl_Group(xoffset,yoffset,xsize,ysize+50,"Torso");
+		{
+			torso_picture_box = new Fl_Box(75+xoffset, 50+yoffset, 100, 100);
+		}
+		torso_g->end();
+
+		Fl_Group *locomotor_g = new Fl_Group(xoffset,yoffset,xsize,ysize+50,"Loco");
+		{
+			locomotor_picture_box = new Fl_Box(75+xoffset, 50+yoffset, 100, 100);
+		}
+		locomotor_g->end();
+
+		Fl_Group *battery_g = new Fl_Group(xoffset,yoffset,xsize,ysize+50,"Batt");
+		{
+			battery_picture_box = new Fl_Box(75+xoffset, 50+yoffset, 100, 100);
+		}
+		battery_g->end();
+
+		Fl_Group *arm_g = new Fl_Group(xoffset,yoffset,xsize,ysize+50,"Arm ");
+		{
+			arm_picture_box = new Fl_Box(75+xoffset, 50+yoffset, 100, 100);
+		}
+		arm_g->end();
+	}
+	tabs->end();
+}
+
+void ModelTabViewer::display_image(RobotModel modelin)
+{
+	head_x = modelin.gethead();
+	locomotor_x = modelin.getlocomotor();
+	torso_x = modelin.gettorso();
+	arm_x = modelin.getarm();
+	battery_x = modelin.getbattery();
+
+	head_image = new Fl_PNG_Image(head_x.getImageLocation().c_str());
+	head_picture_box->image(head_image);
+	head_picture_box->show();
+
+	torso_image = new Fl_PNG_Image(torso_x.getImageLocation().c_str());
+	torso_picture_box->image(torso_image);
+	torso_picture_box->show();
+
+	locomotor_image = new Fl_PNG_Image(locomotor_x.getImageLocation().c_str());
+	locomotor_picture_box->image(locomotor_image);
+	locomotor_picture_box->show();
+
+	battery_image = new Fl_PNG_Image(battery_x.getImageLocation().c_str());
+	battery_picture_box->image(battery_image);
+	battery_picture_box->show();
+
+	arm_image = new Fl_PNG_Image(arm_x.getImageLocation().c_str());
+	arm_picture_box->image(arm_image);
+	arm_picture_box->show();
+
+}
---
+++ code/RRS_Manager_FLTK/RRS_Manager_FLTK/model_tab_viewer.h
@@ -0,0 +1,60 @@
+#ifndef MODEL_TAB_VIEWER_H
+#define MODEL_TAB_VIEWER_H 2016
+
+#include <string>
+#include <vector> 
+#include <cstdlib>
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Menu_Bar.H>
+#include <FL/Fl_Menu_Item.H>
+#include <FL/Fl_Box.H>
+#include <FL/fl_ask.H>
+#include <FL/Fl_PNG_Image.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Int_Input.H>
+#include <FL/Fl_Float_Input.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Choice.H>
+#include <FL/Fl_Group.H>
+#include <FL/Fl_Tabs.H>
+#include "robot_parts.h"
+#include "robot_models.h"
+#include "part_type.h"
+#include "shop.h"
+#include "view.h"
+#include "window_properties.h"
+
+class ModelTabViewer
+{
+public:
+	void display_tabs();
+	void display_image(RobotModel modelin);
+
+private:
+	//Formatting
+	const int tb_offset = 170;
+	const int xoffset = 220+tb_offset, yoffset = 0, xsize = 200, ysize = 200;
+
+	Fl_Tabs *tabs;
+
+	Head head_x;
+	Locomotor locomotor_x;
+	Torso torso_x;
+	Battery battery_x;
+	Arm arm_x;
+
+	Fl_Box* head_picture_box;
+	Fl_Box* torso_picture_box;
+	Fl_Box* locomotor_picture_box;
+	Fl_Box* battery_picture_box;
+	Fl_Box* arm_picture_box;
+
+	Fl_PNG_Image* head_image;
+	Fl_PNG_Image* torso_image;
+	Fl_PNG_Image* locomotor_image;
+	Fl_PNG_Image* battery_image;
+	Fl_PNG_Image* arm_image;
+	
+};
+#endif
